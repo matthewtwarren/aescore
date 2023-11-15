54e5a7657da045ace8296cf383732bffa35339c2
matthewtwarren <mattwarren95@gmail.com>
2023-11-15 14:12:26 +0000

    Add fine tune example

---
+++ examples/finetune.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mkdir -p ftout
+
+python -m ael.finetune \
+    Finetune \
+    ./out/best_0.pth \
+    ./tests/testdata/systems.dat \
+    ./tests/testdata/systems.dat \
+    -t ./tests/testdata/systems.dat \
+    -d ./tests/testdata \
+    -av out/aevc.pth \
+    -am out/amap.json \
+    -cm out/cmap.json \
+    -r 3.5 \
+    -b 2 \
+    -e 5 \
+    --removeHs \
+    --plot \
+    -o ftout
